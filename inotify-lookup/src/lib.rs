c326a392ba7eb777eb9961f67a1f702687b5dfa8
iamhyc <sudofree@163.com>
2021-04-12 11:09:44 +0800

    update dependency

--- inotify-lookup/src/lib.rs
+++ inotify-lookup/src/lib.rs
@@ -1,41 +1,49 @@
+use std::error::Error;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-pub mod inotify_lookup {
-    use std::net;
 
-    fn init_socket() -> usize {
+mod _priv {
+    use std::error::Error;
+
+    fn init_socket() -> Result<(), Box<dyn Error>> {
         unimplemented!();
     }
 
-    fn fini_socket() -> usize {
+    fn fini_socket() -> Result<(), Box<dyn Error>> {
         unimplemented!();
     }
 
-    fn send_request() -> usize {
+    fn send_request() {
         unimplemented!();
     }
 
-    fn recv_message() -> usize {
+    fn recv_message() {
         unimplemented!();
     }
+}
 
-    /*-----------------------------------------------------------*/
-    // #[no_mangle] //not for non-Rust usage
-    pub fn inotify_lookup_register(name: &str) -> usize {
-        unimplemented!();
-    }
+#[pyfunction]
+pub fn inotify_lookup_register(name: &str) -> PyResult<usize> {
+    unimplemented!();
+}
 
-    // #[no_mangle] //not for non-Rust usage
-    pub fn inotify_lookup_unregister(name: &str) -> usize {
-        unimplemented!();
-    }
+#[pyfunction]
+pub fn inotify_lookup_unregister(name: &str) -> PyResult<usize> {
+    unimplemented!();
+}
 
-    // #[no_mangle] //not for non-Rust usage
-    pub fn inotify_lookup_dump(name: &str) -> String {
-        unimplemented!();
-    }
+#[pyfunction]
+pub fn inotify_lookup_dump(name: &str) -> PyResult<String> {
+    unimplemented!();
+}
 
+#[pymodule]
+fn inotify_lookup(_py:Python, m:&PyModule) -> PyResult<()> {
+    m.add_function( wrap_pyfunction!(inotify_lookup_register, m)? )?;
+    m.add_function( wrap_pyfunction!(inotify_lookup_unregister, m)? )?;
+    m.add_function( wrap_pyfunction!(inotify_lookup_dump, m)? )?;
+    Ok(())
 }
 
 #[test]
