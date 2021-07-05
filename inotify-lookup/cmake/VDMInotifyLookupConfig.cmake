f7397ad9ec1031bb2c22648d6c3ada9db47a2920
iamhyc <sudofree@163.com>
2021-07-05 10:56:10 +0800

    [inotify-lookup] rm cmake config file

--- inotify-lookup/cmake/VDMInotifyLookupConfig.cmake
+++
@@ -1,2 +0,0 @@
-set(VDMInotifyLookup_INCLUDE_DIR ${CMAKE_INSTALL_PREFIX}/include/${INSTALL_DIRECTORY})
-set(VDMInotifyLookup_LIBRARY ${CMAKE_INSTALL_PREFIX}/lib/${INSTALL_DIRECTORY}/libvdm-inotify-lookup.so)
