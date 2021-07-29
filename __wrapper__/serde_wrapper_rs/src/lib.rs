023713a43f77faa2f4b93501897b33684bf571b4
iamhyc <sudofree@163.com>
2021-07-29 16:29:43 +0800

    complete serde wrapper for rust

--- __wrapper__/serde_wrapper_rs/src/lib.rs
+++ __wrapper__/serde_wrapper_rs/src/lib.rs
@@ -1,27 +1,41 @@
-extern crate serde_json;
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::parse_macro_input;
+use syn::{parse_macro_input, parse_quote};
 
-#[proc_macro]
-pub fn jsonify(input: TokenStream) -> TokenStream {
+#[proc_macro_attribute]
+pub fn jsonify(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let func = parse_macro_input!(input as syn::ItemFn);
-    let sig = func.sig.clone();
+    let mut block = func.block;
+    let mut sig = func.sig.clone();
 
-    // cloned sig change to: inputs with "&String", output with "&String"
-
-    // build deserialize statements
-    let args_ser = quote!();
+    sig.inputs.iter_mut().for_each(|arg|{
+        match arg {
+            syn::FnArg::Typed(pat) => {
+                let (_pat, _ty) = ( pat.pat.clone(), pat.ty.clone() );
+                // change function input type to "String"
+                pat.ty = syn::parse( quote!(String).into() ).unwrap();
+                // build deserialize statements
+                let _stmt: syn::Stmt = parse_quote!(
+                    let #_pat:#_ty = match serde_json::from_str(&#_pat) {
+                        Ok(res) => res,
+                        Err(_) => return "".into() //panic and exit
+                    };
+                ); 
+                block.stmts.insert(0, _stmt);
+            },
+            _ => {}
+        }
+    });
+    // change function output type to "String"
+    sig.output = syn::parse( quote!(->String).into() ).unwrap();
 
     // generate output
     quote!(
-        pub fn #sig {
-            #args_ser
-            let res = { #func.block };
-            // return serialized output
-            serde_json::to_string(res).unwrap_or("".into())
+        pub #sig {
+            let res = { #block };
+            serde_json::to_string(&res).unwrap_or( "".into() )
         }
     ).into()
 }
