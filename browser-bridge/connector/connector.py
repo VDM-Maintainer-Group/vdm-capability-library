bed27dc06c998a5f664dfc4d065b0694057a5843
iamhyc <sudofree@163.com>
2022-09-30 22:56:21 +0800

    update browser bridge design

--- browser-bridge/connector/connector.py
+++ browser-bridge/connector/connector.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import asyncio, threading
+from queue import Queue
 import struct, json, random, string
 from pathlib import Path
 from sys import stdin, stdout
@@ -7,88 +8,140 @@ from time import sleep
 import dbus, dbus.service, dbus.mainloop.glib
 from gi.repository import GLib
 
+from pyvdm.interface import CapabilityLibrary
+xm = CapabilityLibrary.CapabilityHandleLocal('x11-manager')
+
 FILE_NAME_MAP = {
     'connector_chrome': 'google-chrome',
     'connector_firefox': 'firefox-esr',
     'connector_edge': 'microsoft-edge',
 }
 
+async def connect_stdin_stdout():
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(reader)
+    await loop.connect_read_pipe(lambda: protocol, stdin)
+    w_transport, w_protocol = await loop.connect_write_pipe(asyncio.streams.FlowControlMixin, stdout)
+    writer = asyncio.StreamWriter(w_transport, w_protocol, reader, loop)
+    return (reader, writer)
+
 class BrowserConnector:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
         pass
 
-    @staticmethod
-    def __nm_recv() -> dict:
+    async def __nm_recv(self) -> dict:
         RAW_LEN = struct.calcsize('@I')
-        raw_len = stdin.buffer.read(RAW_LEN)
+        raw_len = await self.reader.read(RAW_LEN)
         buf_len = struct.unpack("@I", raw_len)[0]
-        buf = stdin.buffer.read(buf_len).decode('utf-8')
+        buf = await self.reader.read(buf_len).decode('utf-8')
         return json.loads(buf)
 
-    @staticmethod
-    def __nm_send(msg: dict):
+    async def __nm_send(self, msg: dict):
         buf     = json.dumps(msg).encode('utf-8')
         buf_len = struct.pack('@I', len(buf))
-        stdout.buffer.write(buf_len)
-        stdout.buffer.write(buf)
-        stdout.flush()
+        self.writer.write(buf_len)
+        self.writer.write(buf)
+        await self.writer.drain()
         pass
 
-    def nm_request_sync(self, cmd:str) -> str:
+    async def nm_request_sync(self, cmd:str) -> str:
         ctrl_msg = {'req':cmd}
-        self.__nm_send(ctrl_msg)
-        return self.__nm_recv()
-    
-    def open_temp_tabs(self):
-        pass
-
-    def close_temp_tabs(self):
-        pass
-
-    def dump_tabs(self):
-        return self.nm_request_sync('dump')
-
-    def resume_tabs(self):
-        pass
+        await self.__nm_send(ctrl_msg)
+        return await self.__nm_recv()
 
     pass
 
-class BrowserBridgeInterface(dbus.service.Object):
-    def __init__(self, name) -> None:
-        self.name = name
-        self.unique = ''.join(random.choices(string.ascii_letters, k=5))
-        bus_name = f'org.VDMCompatible.{self.name}.{self.unique}'
+class BrowserWindowInterface(dbus.service.Object):
+    def __init__(self, name, w_id, tx_q, rx_q) -> None:
+        self.name, self.w_id = name, w_id
+        self.tx_q, self.rx_q = tx_q, rx_q
         ##
-        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.xid = 0
+        self.set_xid()
+        self.unique = ''.join(random.choices(string.ascii_letters, k=5))
+        ##
+        bus_name = f'org.VDMCompatible.{self.name}.{self.unique}'
         conn = dbus.service.BusName(bus_name, bus=dbus.SessionBus())
         super().__init__(conn, '/')
+        pass
+
+    def sync_ctrl(self, cmd):
+        self.tx_q.put(cmd)
+        ret = self.rx_q.get() #blocking
+        return json.dumps(ret)
+
+    def set_xid(self):
+        temp_name = f'{self.name}-{self.unique}'
+        ret = self.sync_ctrl({'req':'open_temp', 'w_id':self.w_id, 'name':temp_name})
+        t_id = ret['t_id']
+        try:
+            _window = xm.get_windows_by_name(temp_name)[0]
+            self.xid = _window['xid']
+        except:
+            self.xid = 0
+        finally:
+            self.sync_ctrl({'req':'close_temp', 'w_id':self.w_id, 't_id':t_id})
         pass
 
     @dbus.service.method(dbus_interface='org.VDMCompatible.src',
                         out_signature='s')
     def Save(self) -> str:
-        ret = BrowserConnector('test').dump_tabs()
-        return json.dumps(ret)
+        ret = self.sync_ctrl({'req':'save', 'w_id':self.w_id})
+        return ret
     
     @dbus.service.method(dbus_interface='org.VDMCompatible.src',
                         in_signature='s')
     def Resume(self, stat:str):
-        pass
+        ret = self.sync_ctrl({'req':'resume', 'w_id':self.w_id, 'stat':stat})
+        return ret
 
     @dbus.service.method(dbus_interface='org.VDMCompatible.src')
     def Close(self):
+        self.sync_ctrl({'req':'close', 'w_id':self.w_id})
         pass
 
+    @dbus.service.method(interface=dbus.PROPERTIES_IFACE,
+                         in_signature='ss', out_signature='v')
+    def Get(self, interface_name, property_name):
+        if interface_name=='org.VDMCompatible.src' and property_name=='xid':
+            return self.xid
+        else:
+            raise dbus.exceptions.DBusException('com.example.UnknownInterface',
+                    'interface %s not found.'%interface_name)
+        pass
+
+    @dbus.service.method(interface=dbus.PROPERTIES_IFACE,
+                         in_signature='s', out_signature='a{sv}')
+    def GetAll(self, interface_name):
+        if interface_name == 'org.VDMCompatible.src':
+            return { 'xid' : self.xid }
+        else:
+            raise dbus.exceptions.DBusException('com.example.UnknownInterface',
+                    'interface %s not found.'%interface_name)
+        pass
     pass
 
 def start_glib_thread():
-    BrowserBridgeInterface('test')
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     GLib.MainLoop().run()
 
 async def handle_event(browser_name):
+    (reader, writer) = await connect_stdin_stdout()
+    connector = BrowserConnector(reader, writer)
+    recv_queue = Queue()
+    ##
     while True:
-        await asyncio.sleep(1)
+        #TODO: handle events
+        ##
+        try:
+            req = recv_queue.get_nowait()
+        except:
+            pass
+
+        await asyncio.sleep(0)
     pass
 
 def main():
--- browser-bridge/extension/app/scripts/background.js
+++ browser-bridge/extension/app/scripts/background.js
@@ -2,39 +2,80 @@ browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-// browser.tabs.onUpdated.addListener(async (tabId) => {
-//   browser.pageAction.show(tabId)
-// })
-
 let port = browser.runtime.connectNative('org.vdm.browser_bridge');
 console.log(port)
 
+browser.windows.onCreated.addListener((window) => {
+    if (window.type=='normal') {
+        res = {'w_id':window.id, 'res':'event', 'name':'window_created'}
+        port.postMessage( JSON.stringify(res) )
+    }
+});
+
+browser.windows.onRemoved.addListener((window) => {
+    if (window.type=='normal') {
+        res = {'w_id':window.id, 'res':'event', 'name':'window_removed'}
+        port.postMessage( JSON.stringify(res) )
+    }
+});
+
 port.onMessage.addListener((msg) => {
-  switch(msg['req']) {
-    case 'dump':
-      browser.tabs.query({}, (results) => {
-        res = {}
-        results.forEach(tab => {
-          _item = {'index': tab.index, 'title':tab.title, 'url': tab.url}
-          if (Array.isArray(res[tab.windowId])) {
-            res[tab.windowId].push(_item)
-          }
-          else {
-            res[tab.windowId] = [_item]
-          }
-        });
-        port.postMessage( JSON.stringify(res) );
-      })
-      break;
-    case 'resume':
-      break;
-    case 'exist':
-      break;
-    case 'open_temp':
-      break;
-    case 'close_temp':
-      break;
-    default:
-      break;
-  }
-})
+    let w_id = msg['w_id'];
+    let ret = -1;
+
+    switch(msg['req']) {
+        case 'save':
+            browser.tabs.query({ 'windowId':w_id })
+            .then((tabs) => {
+                ret = tabs
+            }, (err) => {
+                console.log(err)
+            })
+            break;
+        case 'resume':
+            let stat = JSON.parse( msg['stat'] )
+            let (x,y,h,w) = stat['xyhw']
+            browser.windows.update(w_id, {
+                'incognito':stat['incognito'], 'url':stat['url'],
+                'left':x, 'top':y, 'height':h, 'width':w
+            }, (details) => {
+                ret = 0
+            }, (err) => {
+                console.log(err)
+            });
+            break;
+        case 'close':
+            browser.windows.remove(w_id)
+            .then(() => {
+                ret = 0
+            }, (err) => {
+                console.log(err)
+            })
+            break;
+        
+        case 'open_temp':
+            tab_name = msg['name']
+            browser.tabs.create({
+                'title':tab_name, 'discarded':true, 'active':true
+            })
+            .then((tab) => {
+                ret = tab.id
+            }, (err) => {
+                console.log(err)
+            });
+            break;
+        //
+        case 'close_temp':
+            tab_id = msg['t_id']
+            browser.tabs.remove( tab_id )
+            .then(() => {
+                ret = 0
+            }, (err) => {
+                console.log(err)
+            });
+            break;
+    };
+
+    res = {'w_id':w_id, 'res':ret}
+    postMessage.postMessage( JSON.stringify(res) )
+});
