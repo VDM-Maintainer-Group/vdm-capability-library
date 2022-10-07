b9a6073ff4d54ee6a35b6a5576436975ef9dab42
iamhyc <sudofree@163.com>
2022-10-07 17:20:00 +0800

    update script

--- browser-bridge/scripts/install_extension.sh
+++ browser-bridge/scripts/install_extension.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+XPI_ID='browser-bridge@vdm-compatible.org'
+CRX_ID='phmlhncfebmikfmkfombdjnkjmkpdjdl'
+
 # for firefox
+FIREFOX_FOLDER='/usr/lib/mozilla/extensions'
+sudo mkdir -p $FIREFOX_FOLDER/{$XPI_ID}
+for x in ../extension/packages/*.xpi; do
+    sudo cp "$x" $FIREFOX_FOLDER/{$XPI_ID}/$XPI_ID.xpi
+done
 
 # for chrome
-CRX_ID='phmlhncfebmikfmkfombdjnkjmkpdjdl'
-sudo mkdir -p /opt/google/chrome/extensions/
-sudo cp ../extension/packages/$CRX_ID.json /opt/google/chrome/extensions/
-sudo sed -i 's@~@'"$HOME"'@' /opt/google/chrome/extensions/$CRX_ID.json
+CHROME_FOLDER='/opt/google/chrome/extensions'
+sudo mkdir -p $CHROME_FOLDER/
+sudo cp ../extension/packages/chrome.json $CHROME_FOLDER/$CRX_ID.json
+sudo sed -i 's@~@'"$HOME"'@' $CHROME_FOLDER/$CRX_ID.json
 
 # for edge
+EDGE_FOLDER='/usr/share/microsoft-edge/extensions'
+sudo mkdir -p $EDGE_FOLDER/
+sudo cp ../extension/packages/chrome.json $EDGE_FOLDER/$CRX_ID.json
+sudo sed -i 's@~@'"$HOME"'@' $EDGE_FOLDER/$CRX_ID.json
