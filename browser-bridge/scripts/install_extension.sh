5962b7fab30b569985d2295fa9b498d7b1db1c8b
iamhyc <sudofree@163.com>
2022-10-07 16:45:33 +0800

    update script

--- browser-bridge/scripts/install_extension.sh
+++ browser-bridge/scripts/install_extension.sh
@@ -3,7 +3,9 @@
 # for firefox
 
 # for chrome
+CRX_ID='phmlhncfebmikfmkfombdjnkjmkpdjdl'
 sudo mkdir -p /opt/google/chrome/extensions/
-sudo cp ../extension/packages/phmlhncfebmikfmkfombdjnkjmkpdjdl.json /opt/google/chrome/extensions/
+sudo cp ../extension/packages/$CRX_ID.json /opt/google/chrome/extensions/
+sudo sed -i 's@~@'"$HOME"'@' /opt/google/chrome/extensions/$CRX_ID.json
 
 # for edge
