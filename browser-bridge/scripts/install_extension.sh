c5064e0f4443b9d76015bc5d3e5217709a563725
iamhyc <sudofree@163.com>
2022-10-07 15:43:48 +0800

    update chrome id

--- browser-bridge/scripts/install_extension.sh
+++ browser-bridge/scripts/install_extension.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+# for firefox
+
+# for chrome
+sudo mkdir -p /opt/google/chrome/extensions/
+sudo cp ../extension/packages/phmlhncfebmikfmkfombdjnkjmkpdjdl.json /opt/google/chrome/extensions/
+
+# for edge
