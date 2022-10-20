da06b7d84f07d16f2180f0e5cc0c3ae106f23a5c
iamhyc <sudofree@163.com>
2022-10-20 22:05:43 +0800

    fix browser-bridge build

--- browser-bridge/scripts/install_extension.sh
+++ browser-bridge/scripts/install_extension.sh
@@ -5,7 +5,7 @@ CRX_ID='phmlhncfebmikfmkfombdjnkjmkpdjdl'
 
 # for firefox
 FIREFOX_FOLDER='/usr/lib/mozilla/extensions'
-sudo mkdir -p $FIREFOX_FOLDER/{$XPI_ID}
+mkdir -p $FIREFOX_FOLDER/{$XPI_ID}
 for x in ../extension/packages/*.xpi.zip; do
     cp "$x" "${x%.zip}"
     firefox-esr "${x%.zip}" & disown
@@ -14,12 +14,12 @@ done
 
 # for chrome
 CHROME_FOLDER='/opt/google/chrome/extensions'
-sudo mkdir -p $CHROME_FOLDER/
-sudo cp ../extension/packages/chrome.json $CHROME_FOLDER/$CRX_ID.json
-sudo sed -i 's@~@'"$HOME"'@' $CHROME_FOLDER/$CRX_ID.json
+mkdir -p $CHROME_FOLDER/
+cp ../extension/packages/chrome.json $CHROME_FOLDER/$CRX_ID.json
+sed -i 's@~@'"$HOME"'@' $CHROME_FOLDER/$CRX_ID.json
 
 # for edge
 EDGE_FOLDER='/usr/share/microsoft-edge/extensions'
-sudo mkdir -p $EDGE_FOLDER/
-sudo cp ../extension/packages/chrome.json $EDGE_FOLDER/$CRX_ID.json
-sudo sed -i 's@~@'"$HOME"'@' $EDGE_FOLDER/$CRX_ID.json
+mkdir -p $EDGE_FOLDER/
+cp ../extension/packages/chrome.json $EDGE_FOLDER/$CRX_ID.json
+sed -i 's@~@'"$HOME"'@' $EDGE_FOLDER/$CRX_ID.json
