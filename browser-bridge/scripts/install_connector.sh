8299fbf240246fb61a8ac14b5798dc23d3519c33
iamhyc <sudofree@163.com>
2022-10-21 14:11:56 +0800

    fix browser-bridge install script

--- browser-bridge/scripts/install_connector.sh
+++ browser-bridge/scripts/install_connector.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
-chmod +x ../connector/connector.py
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$SCRIPT_DIR/..
+
+chmod +x $ROOT_DIR/connector/connector.py
 mkdir -p /opt/browser-bridge
-cp ../connector/connector.py /opt/browser-bridge
+cp $ROOT_DIR/connector/connector.py /opt/browser-bridge
 
 # for chrome
 # mkdir -p ~/.config/google-chrome/NativeMessagingHosts
 ln -sf /opt/browser-bridge/connector.py /opt/browser-bridge/connector_chrome.py
-# cp ../connector/org.vdm.browser_bridge.chrome.json ~/.config/google-chrome/NativeMessagingHosts/org.vdm.browser_bridge.json
+# cp $ROOT_DIR/connector/org.vdm.browser_bridge.chrome.json ~/.config/google-chrome/NativeMessagingHosts/org.vdm.browser_bridge.json
 mkdir -p /etc/opt/chrome/native-messaging-hosts
-cp ../connector/org.vdm.browser_bridge.chrome.json /etc/opt/chrome/native-messaging-hosts/org.vdm.browser_bridge.json
+cp $ROOT_DIR/connector/org.vdm.browser_bridge.chrome.json /etc/opt/chrome/native-messaging-hosts/org.vdm.browser_bridge.json
 
 # for firefox
 ln -sf /opt/browser-bridge/connector.py /opt/browser-bridge/connector_firefox.py
 # mkdir -p ~/.mozilla/native-messaging-hosts
-# cp ../connector/org.vdm.browser_bridge.firefox.json ~/.mozilla/native-messaging-hosts/org.vdm.browser_bridge.json
+# cp $ROOT_DIR/connector/org.vdm.browser_bridge.firefox.json ~/.mozilla/native-messaging-hosts/org.vdm.browser_bridge.json
 mkdir -p /usr/lib/mozilla/native-messaging-hosts
-cp ../connector/org.vdm.browser_bridge.firefox.json /usr/lib/mozilla/native-messaging-hosts/org.vdm.browser_bridge.json
+cp $ROOT_DIR/connector/org.vdm.browser_bridge.firefox.json /usr/lib/mozilla/native-messaging-hosts/org.vdm.browser_bridge.json
 
 # for microsoft edge
 ln -sf /opt/browser-bridge/connector.py /opt/browser-bridge/connector_edge.py
 # mkdir -p ~/.config/microsoft-edge/NativeMessagingHosts
-# cp ../connector/org.vdm.browser_bridge.edge.json ~/.config/microsoft-edge/NativeMessagingHosts/org.vdm.browser_bridge.json
+# cp $ROOT_DIR/connector/org.vdm.browser_bridge.edge.json ~/.config/microsoft-edge/NativeMessagingHosts/org.vdm.browser_bridge.json
 mkdir -p /etc/opt/edge/native-messaging-hosts
-cp ../connector/org.vdm.browser_bridge.edge.json /etc/opt/edge/native-messaging-hosts/org.vdm.browser_bridge.json
+cp $ROOT_DIR/connector/org.vdm.browser_bridge.edge.json /etc/opt/edge/native-messaging-hosts/org.vdm.browser_bridge.json
--- browser-bridge/scripts/install_extension.sh
+++ browser-bridge/scripts/install_extension.sh
@@ -3,10 +3,13 @@
 XPI_ID='browser-bridge@vdm-compatible.org'
 CRX_ID='phmlhncfebmikfmkfombdjnkjmkpdjdl'
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$SCRIPT_DIR/..
+
 # for firefox
 FIREFOX_FOLDER='/usr/lib/mozilla/extensions'
 mkdir -p $FIREFOX_FOLDER/{$XPI_ID}
-for x in ../extension/packages/*.xpi.zip; do
+for x in $ROOT_DIR/extension/packages/*.xpi.zip; do
     cp "$x" "${x%.zip}"
     firefox-esr "${x%.zip}" & disown
     break
@@ -15,11 +18,11 @@ done
 # for chrome
 CHROME_FOLDER='/opt/google/chrome/extensions'
 mkdir -p $CHROME_FOLDER/
-cp ../extension/packages/chrome.json $CHROME_FOLDER/$CRX_ID.json
+cp $ROOT_DIR/extension/packages/chrome.json $CHROME_FOLDER/$CRX_ID.json
 sed -i 's@~@'"$HOME"'@' $CHROME_FOLDER/$CRX_ID.json
 
 # for edge
 EDGE_FOLDER='/usr/share/microsoft-edge/extensions'
 mkdir -p $EDGE_FOLDER/
-cp ../extension/packages/chrome.json $EDGE_FOLDER/$CRX_ID.json
+cp $ROOT_DIR/extension/packages/chrome.json $EDGE_FOLDER/$CRX_ID.json
 sed -i 's@~@'"$HOME"'@' $EDGE_FOLDER/$CRX_ID.json
