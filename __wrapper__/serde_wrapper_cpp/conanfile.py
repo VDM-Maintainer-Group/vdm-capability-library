c53efcda6ed52b0c0f38883ccf68eefbd2cc8840
iamhyc <sudofree@163.com>
2022-12-14 22:49:51 +0800

    serde cpp remove useless

--- __wrapper__/serde_wrapper_cpp/conanfile.py
+++ __wrapper__/serde_wrapper_cpp/conanfile.py
@@ -1,26 +1,18 @@
-from conans import ConanFile, CMake
+from conans import ConanFile
 
 class SerdeWrapperConan(ConanFile):
     name = "serde_wrapper"
     version = "0.1"
     license = "GPL-3.0"
     author = "iamhyc sudofree@163.com"
-    #
-    settings = "os", "compiler", "arch", "build_type"
-    exports_sources = "include/*", "CMakeLists.txt", "test.cpp"
-    generators = "cmake"
+    url = "https://github.com/VDM-Maintainer-Group/vdm-capability-library"
+    description = "jsonify serde_wrapper for cpp."
     #
     no_copy_source = True
     requires = "rapidjson/cci.20200410"
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-        cmake.test()
-
     def package(self):
-        self.copy("*.hpp")
+        self.copy("*.hpp", "include")
 
     def package_id(self):
         self.info.header_only()
--- __wrapper__/serde_wrapper_cpp/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "serde_wrapper.hpp"
-
-int main()
-{
-    std::cout << "build pass so test pass" << std::endl;
-}
